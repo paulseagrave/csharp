10da8542b152b5587431344594312afd726fea7a
Paul Seagrave <paul.seagrave@googlemail.com>
2018-05-17 17:09:28 +0100

    Initial commit

---
+++ SimpleAsync/SimpleAsync/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SimpleAsync
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DoWork();
+        }
+
+        private static async Task DoWork()
+        {
+            await GetAsyncResults();
+            Console.WriteLine("Done.\nPress any key to exit . . .");
+            Console.ReadLine();
+        }
+
+        private static async Task GetAsyncResults()
+        {
+            IEnumerable<Task<Results>> allTasks =
+                from i in Enumerable.Range(1, 10) select GetAsyncTask(i);
+
+            List<Task<Results>> taskList = allTasks.ToList();
+
+            while (taskList.Count > 0)
+            {
+                Task<Results> finishedTask = await Task.WhenAny(taskList);
+
+                taskList.Remove(finishedTask);
+                Results result = await finishedTask;
+                Console.WriteLine(result.Message);
+            }
+        }
+
+        private static Task<Results> GetAsyncTask(int i)
+        {
+            Func<Results> func = () =>
+            {
+
+
+                Random rand = new Random();
+                int sleepMillis = rand.Next(1000,5000);
+                Thread.Sleep(sleepMillis);
+                return new Results()
+                {
+                    Index = i,
+                    Message = String.Format("{0} Slept for {1} milliseconds", i, sleepMillis)
+                };
+            };
+
+            var t = new Task<Results>(func);
+            t.Start();
+            return t;
+        }
+    }
+
+    internal class Results
+    {
+        public int Index { get; set; }
+        public string Message { get; set; }
+    }
+}
